0d688f1fb3d83d5ae5af4aa5a0ad468c8627ceae
Nick Bolton <nick@symless.com>
2025-05-02 16:18:43 +0100

    First prototype (with example build-command.json)

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+interface BuildCommand {
+  command: string;
+  branch: string;
+  timestamp: number;
+}
+
+let lastTimestamp = 0;
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "multi-host-build" is now active!');
+  const command = "multiHostCmake.startWatcher";
+  const disposable = vscode.commands.registerCommand(command, () => {
+    vscode.window.showInformationMessage("Multi-Host Watcher Started");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("multi-host-build.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from multi-host-build!");
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      vscode.window.showErrorMessage("No workspace open");
+      return;
+    }
+
+    const watchPath = path.join(workspaceFolders[0].uri.fsPath, "build-command.json");
+
+    fs.watchFile(watchPath, { interval: 1000 }, async () => {
+      try {
+        const content = fs.readFileSync(watchPath, "utf8");
+        const data = JSON.parse(content) as BuildCommand;
+
+        if (data.timestamp > lastTimestamp && data.command === "build") {
+          lastTimestamp = data.timestamp;
+
+          vscode.window.showInformationMessage(`Running build for branch: ${data.branch}`);
+
+          await vscode.commands.executeCommand("git.checkout", data.branch);
+          await vscode.commands.executeCommand("cmake.configure");
+          await vscode.commands.executeCommand("cmake.build");
+        }
+      } catch (err) {
+        console.error("Watcher error:", err);
+      }
+    });
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
